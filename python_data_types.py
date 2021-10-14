9a8d10a1e784c1d0cdb4a4c779874ead98271a90
jurkatom <79424611+jurkatom@users.noreply.github.com>
2021-10-14 15:40:08 +0200

    Update python_data_types.py

--- python_data_types.py
+++ python_data_types.py
@@ -1,1 +1,6 @@
-First line (testing)
+#1) Dědictví (11)
+heritage = 1256983;
+childrenNum = 28;
+remaining = heritage % childrenNum;
+print(remaining)
+print("King will have " + remaining + " coins remaining.");
